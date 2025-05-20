5f25c91f0c78ef6c044fde7e91d3ddf5c1e344a5
sbaris <sbarisemre@gmail.com>
2025-05-20 10:21:50 +0300

    BusSync Server

---
+++ server/src/main/java/com/example/reservationudemy/controller/BusRouteController.java
@@ -0,0 +1,43 @@
+package com.example.reservationudemy.controller;
+
+import com.example.reservationudemy.entities.BusRoute;
+import com.example.reservationudemy.models.ResponseModel;
+import com.example.reservationudemy.services.BusRouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/route")
+public class BusRouteController {
+
+    @Autowired
+    private BusRouteService busRouteService;
+
+    @PostMapping("/add")
+    public ResponseModel<BusRoute> addRoute(@RequestBody BusRoute busRoute) {
+        final BusRoute busRoute1 = busRouteService.addRoute(busRoute);
+        return new ResponseModel<>(HttpStatus.OK.value(), "Route Saved", busRoute1);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<BusRoute>> getAllRoutes() {
+        return ResponseEntity.ok(busRouteService.getAllBusRoutes());
+    }
+
+    @GetMapping("/{routeName}")
+    public ResponseEntity<BusRoute> getRouteByRouteName(@PathVariable(name = "routeName") String routeName) {
+        return ResponseEntity.ok(busRouteService.getRouteByRouteName(routeName));
+    }
+
+    @GetMapping("/query")
+    public ResponseEntity<BusRoute> getRouteByCityFromAndCityTo(
+            @RequestParam String cityFrom,
+            @RequestParam String cityTo
+    ) {
+        return ResponseEntity.ok(busRouteService.getRouteByCityFromAndCityTo(cityFrom, cityTo));
+    }
+}
---
+++ server/src/main/java/com/example/reservationudemy/services/BusRouteService.java
@@ -0,0 +1,14 @@
+package com.example.reservationudemy.services;
+
+import com.example.reservationudemy.entities.BusRoute;
+
+import java.util.List;
+
+public interface BusRouteService {
+    BusRoute addRoute(BusRoute busRoute);
+    List<BusRoute> getAllBusRoutes();
+
+    BusRoute getRouteByRouteName(String routeName);
+
+    BusRoute getRouteByCityFromAndCityTo(String cityFrom, String cityTo);
+}
